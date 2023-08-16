16946fc3e9596e6d7387f659e6d14f7a71cf0614
Ankit Raj <ankit.raj928@gmail.com>
2023-08-16 16:11:04 +0000

    added debouncing

--- src/components/StockPicker.js
+++ src/components/StockPicker.js
@@ -1,99 +1,5 @@
-/*import React from "react";
-import "./StockPicker.css";
-import { useSelector, useDispatch } from "react-redux";
-import {
-  setSearchTerm,
-  setAutocompleteResults,
-  setSelectedStock
-} from "../actions/action";
-
-const stockData = [
-  {
-    name: "Apple Inc.",
-    symbol: "AAPL",
-    description: "Apple Inc. is a multinational technology company...",
-    current_price: 150.25,
-    change: "+2.18%",
-    traded_on: "NASDAQ",
-    industry: "Technology",
-    pe_ratio: 28.45,
-    market_cap: "2.5T"
-  },
-  {
-    name: "Amazon.com Inc.",
-    symbol: "AMZN",
-    description: "Amazon.com, Inc. is an American e-commerce and...",
-    current_price: 3250.75,
-    change: "-1.05%",
-    traded_on: "NASDAQ",
-    industry: "Retail",
-    pe_ratio: 72.91,
-    market_cap: "1.6T"
-  },
-  {
-    name: "Microsoft Corporation",
-    symbol: "MSFT",
-    description: "Microsoft Corporation is a multinational technology...",
-    current_price: 305.4,
-    change: "+0.75%",
-    traded_on: "NASDAQ",
-    industry: "Technology",
-    pe_ratio: 37.89,
-    market_cap: "2.3T"
-  },
-  {
-    name: "Tesla, Inc.",
-    symbol: "TSLA",
-    description: "Tesla, Inc. is an American electric vehicle and clean...",
-    current_price: 720.6,
-    change: "-0.50%",
-    traded_on: "NASDAQ",
-    industry: "Automotive",
-    pe_ratio: 176.25,
-    market_cap: "750B"
-  }
-  // Add more stock entries as needed
-];
-
-function StockPicker() {
-  const state = useSelector((state) => state);
-  const dispatch = useDispatch();
-
-  const handleSearchTermChange = (event) => {
-    const newSearchTerm = event.target.value;
-    dispatch(setSearchTerm(newSearchTerm));
-
-    if (newSearchTerm.trim() === "") {
-      dispatch(setAutocompleteResults([]));
-      return;
-    }
-
-    const filteredStocks = stockData.filter((stock) =>
-      stock.symbol.toLowerCase().includes(newSearchTerm.toLowerCase())
-    );
-
-    dispatch(setAutocompleteResults(filteredStocks));
-  };
-
-  const handleAutocompleteClick = (stock) => {
-    dispatch(setSelectedStock(stock));
-    dispatch(setAutocompleteResults([]));
-    dispatch(setSearchTerm(""));
-  };
-
-  const handleSearch = () => {
-  
-    const foundStock = stockData.find(
-      (stock) => stock.symbol.toLowerCase() === state.searchTerm.toLowerCase()
-    );
-
-    dispatch(setSelectedStock(foundStock));
-    dispatch(setSearchTerm(""));
-  };
-
-  */
-
 import React, { useState, useEffect } from "react";
+import _debounce from "lodash/debounce";
 import "./StockPicker.css";
 import { useSelector, useDispatch } from "react-redux";
 import {
@@ -108,7 +14,7 @@ function StockPicker() {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    async function fetchAutocomplete() {
+    const fetchAutocomplete = async () => {
       if (state.searchTerm.trim() === "") {
         dispatch(setAutocompleteResults([]));
         return;
@@ -118,7 +24,7 @@ function StockPicker() {
 
       try {
         const response = await fetch(
-          `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${state.searchTerm}&apikey=6AVC3L6F4WXVVLNI`
+          `https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=${state.searchTerm}&apikey=YOUR_API_KEY`
         );
         const data = await response.json();
 
@@ -135,9 +41,15 @@ function StockPicker() {
       }
 
       setLoading(false);
-    }
+    };
 
-    fetchAutocomplete();
+    const debouncedFetchAutocomplete = _debounce(fetchAutocomplete, 300);
+
+    debouncedFetchAutocomplete(); // Call the debounced function
+
+    return () => {
+      debouncedFetchAutocomplete.cancel(); // Cancel the debounce function on unmount
+    };
   }, [state.searchTerm, dispatch]);
 
   const handleAutocompleteClick = async (symbol) => {
@@ -145,7 +57,7 @@ function StockPicker() {
 
     try {
       const response = await fetch(
-        `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=6AVC3L6F4WXVVLNI`
+        `https://www.alphavantage.co/query?function=OVERVIEW&symbol=${symbol}&apikey=YOUR_API_KEY`
       );
       const data = await response.json();
 
